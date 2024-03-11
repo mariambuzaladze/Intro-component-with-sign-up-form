ddb911980dafaabbce93365a81fd0c2b8ec0bda2
mariambuzaladze <mariambuzaladze10@example.come>
2024-03-12 00:10:45 +0400

    add input functionality

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,5 +1,16 @@
 import "./input.css";
 
-export default function Input({ placeholder, id }) {
-  return <input id={id} type="text" placeholder={placeholder} />;
+export default function Input({ placeholder, id, onChange, text, className }) {
+  return (
+    <div className="input">
+      <input
+        id={id}
+        type="text"
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+
+      <p className={className}>{text}</p>
+    </div>
+  );
 }
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,13 +1,72 @@
 import "./signUp.css";
 import Input from "../Input/Input";
+import Error from "/images/icon-error.svg";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [firstName, setFirstName] = useState({
+    value: "",
+    className: "error-text",
+  });
+  const [secondName, setSecondName] = useState({
+    value: "",
+    className: "error-text",
+  });
+  const [email, setEmail] = useState({ value: "", className: "error-text" });
+  const [password, setPassword] = useState({
+    value: "",
+    className: "error-text",
+  });
+
+  const displayError = (event, setState) => {
+    let value = event.target.value.trim();
+    if (value.length === 0) {
+      setState({ value: Error, className: "error-active" });
+    } else {
+      setState({ value: "", className: "error-text" });
+    }
+  };
+
+  const displayErrorEmail = (event, setState) => {
+    let value = event.target.value.trim();
+    if (value.length === 0 || !value.split("").includes("@")) {
+      setState({ value: Error, className: "error-active" });
+    } else {
+      setState({ value: "", className: "error-text" });
+    }
+  };
+
   return (
     <div className="sign-up">
-      <Input id="1" placeholder="First Name" />
-      <Input id="2" placeholder="Second Name" />
-      <Input id="3" placeholder="Email Address" />
-      <Input id="4" placeholder="Password" />
+      <Input
+        id="1"
+        placeholder="First Name"
+        text="First Name cannot be empty"
+        onChange={(event) => displayError(event, setFirstName)}
+        className={firstName.className}
+      />
+      <Input
+        id="2"
+        placeholder="Second Name"
+        text="Second Name cannot be empty"
+        onChange={(event) => displayError(event, setSecondName)}
+        className={secondName.className}
+      />
+      <Input
+        id="3"
+        placeholder="Email Address"
+        text="Looks like this is not an email"
+        onChange={(event) => displayErrorEmail(event, setEmail)}
+        className={email.className}
+      />
+      <Input
+        id="4"
+        placeholder="Password"
+        text="Password cannot be empty"
+        onChange={(event) => displayError(event, setPassword)}
+        className={password.className}
+      />
+
       <div className="button">
         <button>CLAIM YOUR FREE TRIAL</button>
         <p className="button-text">
