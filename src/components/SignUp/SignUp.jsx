8c0e600e9cf56cf76a1af797515cded24643de51
mariambuzaladze <mariambuzaladze10@example.come>
2024-03-11 22:04:21 +0400

    add signUp elements

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,5 +1,19 @@
 import "./signUp.css";
+import Input from "../Input/Input";
 
 export default function SignUp() {
-  return;
+  return (
+    <div className="sign-up">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <div className="button">
+        <button>CLAIM YOUR FREE TRIAL</button>
+        <p className="button-text">
+          By clicking the button, you are agreeing to our Terms and Services
+        </p>
+      </div>
+    </div>
+  );
 }
